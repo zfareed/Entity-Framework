18cf7d6887b02b4a1abdb4dd301bf62cdb16f058
Zain Fareed <zainfareed093@gmail.com>
2020-05-21 16:49:32 +0500

    Completed Lesson 7 (Demo: Code-First WorkFlow)

---
+++ DatabaseFirstDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DatabaseFirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new DataBaseFirstDemoEntities();
+            var post = new Post()
+            {
+                Body = "Body1",
+                PostID = 1,
+                DatePublished = DateTime.Now,
+                Title = "Title1"
+            };
+            context.Posts.Add(post);
+            context.SaveChanges();
+
+
+
+        }
+    }
+}
