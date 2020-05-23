67a0b1a17c696fc7c9afae5550eb7419b61f1811
Zain Fareed <zainfareed093@gmail.com>
2020-05-23 22:04:10 +0500

    Completed Lesson 24  (Codefirst With an exesting) Database

---
+++ PlutoCodeFirst/Model1.cs
@@ -0,0 +1,32 @@
+namespace PlutoCodeFirst
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class Model1 : DbContext
+    {
+        public Model1()
+            : base("name=PlutoContext")
+        {
+        }
+
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Cours> Courses { get; set; }
+        public virtual DbSet<Tag> Tags { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author>()
+                .HasMany(e => e.Courses)
+                .WithOptional(e => e.Author)
+                .HasForeignKey(e => e.Author_Id);
+
+            modelBuilder.Entity<Cours>()
+                .HasMany(e => e.Tags)
+                .WithMany(e => e.Courses)
+                .Map(m => m.ToTable("TagCourses").MapLeftKey("Course_Id"));
+        }
+    }
+}
