dcb0b358d19d8b8f459343ce5a923215b05701df
Zain Fareed <zainfareed093@gmail.com>
2020-05-23 21:21:51 +0500

    Completed Lesson 23 (Building a Model using Code-First Workflow)

---
+++ CodeFirstwithNewDatabase(Pluto)/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace CodeFirstwithNewDatabase_Pluto_.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CodeFirstwithNewDatabase_Pluto_.PlutoContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(CodeFirstwithNewDatabase_Pluto_.PlutoContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
--- CodeFirstwithNewDatabase(Pluto)/Program.cs
+++ CodeFirstwithNewDatabase(Pluto)/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -44,8 +45,15 @@ namespace CodeFirstwithNewDatabase_Pluto_
         public DbSet<Course> Courses { get; set; }
         public DbSet<Author> Authors { get; set; }
         public DbSet<Tag> Tags { get; set; }
+
+        public PlutoContext()
+            : base("name=DefaultConnection")
+        {
+                
+        }
+
     }
-        class Program
+    class Program
     {
         static void Main(string[] args)
         {
